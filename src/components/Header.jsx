cb5b2bc51aaa7410a37930641fbe2737953153b4
alecksv <aleck.svetlovich@gmail.com>
2021-11-23 09:20:58 +0300

    react first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header (){
+    return (
+        
+            <header className="header">
+                <img src="https://png.pngtree.com/png-clipart/20190613/original/pngtree-instagram-icon-logo-png-image_3560506.jpg" />
+            </header>
+        
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Profile (){
+    return (
+      
+        <div className="content">
+            <div>
+              <img src="https://thumbs.dreamstime.com/b/cosmos-beauty-deep-space-elements-image-furnished-nasa-science-fiction-art-102581846.jpg"/>
+              </div>
+            <div>
+              My posts
+            </div>
+            <div>
+               New post
+            </div>        
+          </div>
+      
+    )
+}
+
+export default Profile;
